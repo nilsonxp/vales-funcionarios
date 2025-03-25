5d68ee40c886ae3754933e513ef956238201b531
nilsonxp <nilsonxp@gmail.com>
2025-03-24 23:04:54 -0400

    first commit

---
+++ backend/src/main/java/com/evoxdev/vales_fiados_app/ValesFiadosAppApplication.java
@@ -0,0 +1,13 @@
+package com.evoxdev.vales_fiados_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValesFiadosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValesFiadosAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/evoxdev/vales_fiados_app/ValesFiadosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evoxdev.vales_fiados_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValesFiadosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
